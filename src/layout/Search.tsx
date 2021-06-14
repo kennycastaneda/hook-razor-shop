2ddd5b6bc6337e2b4b8286bf7b766ad0e134fb56
Kenny Castaneda <kenny.castaneda@gmail.com>
2021-06-14 10:27:41 -0500

    search not working

--- src/layout/Search.tsx
+++ src/layout/Search.tsx
@@ -2,11 +2,14 @@ import React from "react";
 
 export default function Search() {
    return (
-      <form className="text-center">
-         <input type="text" className="input" placeholder="Search"></input>
-         <button type="submit" className="btn btn-dark">
-            Search
-         </button>
-      </form>
+      <div>
+         <form className="text-center">
+            <input type="text" className="input" placeholder="Search"></input>
+            <button type="submit" className="btn btn-dark">
+               Search
+            </button>
+         </form>
+         <h3>Not working at the moment.</h3>
+      </div>
    );
 }
