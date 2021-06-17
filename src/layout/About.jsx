d213163bd4f25538c4639e0e3efcb86923ad21c2
Kenny Castaneda <kenny.castaneda@gmail.com>
2021-06-16 21:46:43 -0500

    added about and shop pages...

--- src/layout/About.jsx
+++ src/layout/About.jsx
@@ -4,7 +4,7 @@ export default function Shop(){
 
     return(
         <div className="container p-5 mt-5 d-flex row justify-content-center mx-auto">
-            <div className = "container d-flex row w-75 mx-5 justify-content-center">
+            <div className = "container d-flex row w-sm-100 w-md-75 mx-5 justify-content-center">
                 <div className = "col p-1 ">
                    <img src={process.env.PUBLIC_URL + "/images/hook-about-2.jpeg"} alt="hook shop 1" className="w-100"></img>
                 </div>
@@ -15,7 +15,7 @@ export default function Shop(){
                    <img src={process.env.PUBLIC_URL + "/images/hook-about-1.jpeg"} alt="hook shop 3" className="w-100"></img>
                 </div>
             </div>
-            <div className = "row p-1 w-75 text-center">
+            <div className = "row p-1 w-sm-100 w-md-75 text-center">
                    <img src={process.env.PUBLIC_URL + "/images/3dprintgif.gif"} alt="hook shaving gif"  className = "col w-50"></img>
             </div>
                      
--- src/layout/Shop.jsx
+++ src/layout/Shop.jsx
@@ -4,7 +4,7 @@ export default function Shop(){
 
     return(
         <div className="container p-5 mt-5 d-flex row justify-content-center mx-auto">
-            <div className = "container d-flex row w-50 mx-5 justify-content-center">
+            <div className = "container d-flex row w-sm-100 w-md-75 w-lg-50 mx-5 justify-content-center">
                 <div className = "col p-1 ">
                    <img src={process.env.PUBLIC_URL + "/images/hook-shop-1.jpg"} alt="hook shop 1" className="w-100"></img>
                 </div>
@@ -12,10 +12,10 @@ export default function Shop(){
                   <img src={process.env.PUBLIC_URL + "/images/hook-shop-2.jpg"} alt="hook shop 2" className="w-100"></img>
                 </div>
             </div>
-            <div className = "row p-1 w-50 text-center">
+            <div className = "row p-1 w-sm-100 w-md-75 w-lg-50 text-center">
                    <img src={process.env.PUBLIC_URL + "/images/hook-shaving.gif"} alt="hook shaving gif"  className = "col w-50"></img>
             </div>
-            <div className = "container d-flex row w-50  mx-5 justify-content-center">
+            <div className = "container d-flex row w-sm-100 w-md-75 w-lg-50 mx-5 justify-content-center">
                 <div className = "col p-1">
                    <img src={process.env.PUBLIC_URL + "/images/hook-shop-3.jpg"} alt="hook shop 3" className="w-100"></img>
                 </div>
